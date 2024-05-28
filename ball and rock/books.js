57c58e8e99b2bc3319101b28031789d4a86c2bef
Alexanie <ocxigin@gmail.com>
2024-05-28 18:56:37 +0100

    ball and rock game add

---
+++ ball and rock/books.js
@@ -0,0 +1,55 @@
+const books = [
+    {
+        name: "Don Quixote",
+        author: "Miguel de Cervantes",
+        genre: "Novel",
+        publicationDate: 1605,
+        description: "Don Quixote is a Spanish epic novel by Miguel de Cervantes. It was originally published in two parts, in 1605 and 1615",
+        imgCover: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Title_page_first_edition_Don_Quijote.jpg"
+    },
+
+    {
+        name: "Wuthering Heights",
+        author: "Emily Brontë",
+        genre: "Tragedy, gothic",
+        publicationDate: 1847,
+        description: "It concerns two families of the landed gentry living on the West Yorkshire moors, the Earnshaws and the Lintons, and their turbulent relationships with the Earnshaws' foster son",
+        imgCover: "https://upload.wikimedia.org/wikipedia/commons/6/64/Houghton_Lowell_1238.5_%28A%29_-_Wuthering_Heights%2C_1847.jpg"
+    },
+
+    {
+        name: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        genre: "Tragedy",
+        publicationDate: 1925,
+        description: "Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan.",
+        imgCover: "https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg"
+    },
+
+    {
+        name: "Jane Eyre",
+        author: "Charlotte Brontë",
+        genre: "Gothic Bildungsroman Romance",
+        publicationDate: 1847,
+        description: "Jane Eyre is a bildungsroman that follows the experiences of its eponymous heroine, including her growth to adulthood and her love for Mr Rochester, the brooding master of Thornfield Hall",
+        imgCover: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Jane_Eyre_title_page.jpg"
+    },
+
+    {
+        name: "Moby-Dick",
+        author: "Herman Melville",
+        genre: "Adventure fiction epic sea story encyclopedic novel",
+        publicationDate: 1851,
+        description: "The book is the sailor Ishmael's narrative of the maniacal quest of Ahab, captain of the whaling ship Pequod, for vengeance against Moby Dick, the giant white sperm whale that bit off his leg on the ship's previous voyage",
+        imgCover: "https://upload.wikimedia.org/wikipedia/commons/3/36/Moby-Dick_FE_title_page.jpg"
+    },
+
+    {
+        name: "One Hundred Years of Solitude",
+        author: "Gabriel García Márquez",
+        genre: "Magic realism",
+        publicationDate: 1967,
+        description: "tells the multi-generational story of the Buendía family, whose patriarch, José Arcadio Buendía, founded the fictitious town of Macondo. The novel is often cited as one of the supreme achievements in world literature",
+        imgCover: "https://upload.wikimedia.org/wikipedia/en/a/a0/Cien_a%C3%B1os_de_soledad_%28book_cover%2C_1967%29.jpg"
+    }
+]
